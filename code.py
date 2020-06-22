d4f54bfe83e3c8955e1053b09f74193a2890dd16
BeholdenArt <50458157+BeholdenArt@users.noreply.github.com>
2020-06-22 15:29:44 +0530

    Added Comments
    Added comments in code

--- code.py
+++ code.py
@@ -6,17 +6,17 @@ import time
 def click():
     with mss.mss() as sct:
         while True:
-            img = sct.grab(bbox)
+            img = sct.grab(bbox)    # Taking Screenshot
 
             for coordinate in coordinate_x:
-                if img.pixel(coordinate, 0)[0] < 100:
-                    pyautogui.click(INITIAL_X + coordinate, INITIAL_Y)
+                if img.pixel(coordinate, 0)[0] < 100:    # Checking black & blue pixels
+                    pyautogui.click(INITIAL_X + coordinate, INITIAL_Y)    # Click
                     break
 
 if __name__ == "__main__":
     time.sleep(4)
-    INITIAL_X, INITIAL_Y = 660, 650
+    INITIAL_X, INITIAL_Y = 660, 650    # Boundaries of game window
     bbox = (INITIAL_X, INITIAL_Y, INITIAL_X+540, INITIAL_Y+1)
 
-    coordinate_x = [0, 180, 360, 539]
+    coordinate_x = [0, 180, 360, 539]    # Lanes of tiles (4)
     click()
